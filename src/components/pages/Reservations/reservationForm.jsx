be97efc2bb845b50c4e97baf57f79c8758cefb6b
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2024-11-10 15:14:54 +0100

    add EmailJs

--- src/components/pages/Reservations/reservationForm.jsx
+++ src/components/pages/Reservations/reservationForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import FormField from "./formField";
+import emailjs from '@emailjs/browser';
 
 const ReservationForm = ({
   availableTimes,
@@ -10,24 +11,30 @@ const ReservationForm = ({
   const defaultTime = availableTimes[0];
   const minimumNumberOfGuests = 1;
   const maximumNumberOfGuests = 10;
-  const occasions = ["Birthday", "Anniversary"];
+  const occasions = ["Birthday", "Anniversary", "Engagement", "Other"];
   const invalidDateErrorMessage = "Please choose a valid date";
   const invalidTimeErrorMessage = "Please choose a valid time";
   const invalidOccasionErrorMessage = "Please choose a valid occasion";
   const invalidNumberOfGuestsErrorMessage =
     "Please enter a number between 1 and 10";
 
+  const [name, setName] = useState("");
   const [date, setDate] = useState(minimumDate);
+  const [mail, setMail] = useState("");
   const [time, setTime] = useState(defaultTime);
   const [numberOfGuests, setNumberGuests] = useState(minimumNumberOfGuests);
   const [occasion, setOccasion] = useState(occasions[0]);
 
+  const isNameValid = () => name !== "";
+  const isMailValid = () => mail !== "";
   const isDateValid = () => date !== "";
   const isTimeValid = () => time !== "";
   const isNumberOfGuestsValid = () => numberOfGuests !== "";
   const isOccasionValid = () => occasion !== "";
 
   const areAllFieldsValid = () =>
+    isNameValid() &&
+    isMailValid() &&
     isDateValid() &&
     isTimeValid() &&
     isNumberOfGuestsValid() &&
@@ -38,15 +45,65 @@ const ReservationForm = ({
     dispatchOnDateChange(e.target.value);
   };
 
+  const handleNameChange = (e) => setName(e.target.value);
+  const handleMailChange = (e) => setMail(e.target.value);
   const handleTimeChange = (e) => setTime(e.target.value);
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    submitData({ date, time, numberOfGuests, occasion });
+
+    const service_id = 'service_i3vng1m';
+    const template_id = 'template_ueghurd';
+    const public_id = 'gbtOYXQ6SZQAOaHko';
+
+    const templateParams = {
+      from_name: "Little Lemon",
+      from_email: mail,
+      to_name: name,
+      message: `You have a reservation on ${date} at ${time} for ${numberOfGuests} guests.`,
+    };
+
+    // Send the email using EmailJS
+    emailjs.send(service_id, template_id, templateParams, public_id)
+      .then((response) => {
+        console.log('Email sent successfully:', response);
+      })
+      .catch((error) => {
+        console.error('Error sending email:', error);
+      });
+    submitData({ name, mail, date, time, numberOfGuests, occasion });
   };
 
   return (
     <form onSubmit={handleFormSubmit}>
+      <FormField
+        label="Name"
+        htmlFor="reservation-name"
+        hasError={!isNameValid()}
+      >
+        <input
+          type="text"
+          id="reservation-name"
+          name="reservation-name"
+          value={name}
+          required={true}
+          onChange={handleNameChange}
+        />
+      </FormField>
+      <FormField
+        label="Email address"
+        htmlFor="reservation-mail"
+        hasError={!isMailValid()}
+      >
+        <input
+          type="email"
+          id="reservation-mail"
+          name="reservation-mail"
+          value={mail}
+          required={true}
+          onChange={handleMailChange}
+        />
+      </FormField>
       <FormField
         label="Date"
         htmlFor="reservation-date"
